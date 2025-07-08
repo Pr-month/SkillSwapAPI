b1270d5a34ddbbf0878fe01f8d915b64ff51c277
Olga Kladova <Olenka-29-04@yandex.ru>
2025-07-08 18:11:41 +0300

    feat(users): написаны юнит-тесты, удалены console.log

--- backend/src/auth/guards/role-guard.guard.ts
+++ backend/src/auth/guards/role-guard.guard.ts
@@ -11,7 +11,6 @@ export class RoleGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<AuthRequest>();
     const user = request.user;
-    console.log('RoleGuard user:', user);
     if (user.role !== 'admin') {
       throw new ForbiddenException(
         'Доступ запрещен: требуется роль администратора',
