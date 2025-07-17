6a2c05d41a9ec6bbdc0907d37108ea7222b6c109
Roma Bludilin <romabludilin@yandex.ru>
2025-07-17 18:22:30 +0400

    fix(category): указаны все акутальные данные для Swagger

---
+++ backend/src/categories/dto/findAll-category-response.dto.ts
@@ -0,0 +1,11 @@
+import { Type } from 'class-transformer';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { CreateCategoryDto } from './create-category.dto';
+
+export class FindALLCategoryResponseDto extends PickType(CreateCategoryDto, ['name', 'children']) {
+  @ApiProperty({
+    example: 'd6c5f4e8-1a90-4b1c-b3d2-6e7g8h9i0000',
+    description: 'Уникальный идентификатор категории'
+  })
+  id: string;
+}
