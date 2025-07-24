ffb7b5ec11338d689c1daeaa8b15933f043cb8c3
Alexandr <ntl1221@yandex.ru>
2025-07-24 13:13:56 +0300

    upd request

--- backend/src/requests/requests.controller.ts
+++ backend/src/requests/requests.controller.ts
@@ -32,7 +32,7 @@ export class RequestsController {
   constructor(private readonly requestsService: RequestsService) {}
 
   @ApiBearerAuth('access-token')
-  @ApiOperation({ summary: 'Отправление запроса' })
+  @ApiOperation({ summary: 'Отправление заявки' })
   @UseGuards(AccessTokenGuard)
   @Post()
   create(@Req() req: AuthRequest, @Body() createRequestDto: CreateRequestDto) {
@@ -42,7 +42,7 @@ export class RequestsController {
   @ApiBearerAuth('access-token')
   @UseGuards(AccessTokenGuard)
   @Get('/incoming')
-  @ApiOperation({ summary: 'Получение всех запросов' })
+  @ApiOperation({ summary: 'Получение входящей заявки(только актуальные)' })
   @ApiResponse({
     status: 200,
     description: 'Список всех запросов',
@@ -55,7 +55,7 @@ export class RequestsController {
   @ApiBearerAuth('access-token')
   @UseGuards(AccessTokenGuard)
   @Get('/outgoing')
-  @ApiOperation({ summary: 'Получение всех запросов' })
+  @ApiOperation({ summary: 'Получение исходящие заявки(только актуальные)' })
   @ApiResponse({
     status: 200,
     description: 'Список всех запросов',
@@ -67,23 +67,25 @@ export class RequestsController {
 
   @ApiBearerAuth('access-token')
   @UseGuards(AccessTokenGuard)
-  @Get(':id')
-  findOne(@Req() req: AuthRequest, @Param('id') id: string) {
-    return this.requestsService.findOne(req.user.sub, id, req.user.role);
-  }
-
-  @ApiBearerAuth('access-token')
-  @UseGuards(AccessTokenGuard)
-  @Patch(':id')
+  @Patch(':id/read')
+  @ApiOperation({ summary: 'Возможность прочитать заявку' })
   read(@Param('id') id: string, @Body() updateDto: UpdateRequestDto) {
     return this.requestsService.update(id, updateDto);
   }
 
   @ApiBearerAuth('access-token')
   @UseGuards(AccessTokenGuard)
-  @Delete(':id')
+  @Patch(':id/accept')
+  @ApiOperation({ summary: 'Возможность принять заявку' })
+  rad(@Param('id') id: string, @Body() updateDto: UpdateRequestDto) {
+    return this.requestsService.update(id, updateDto);
+  }
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(AccessTokenGuard)
+  @Patch(':id/reject')
   @ApiOperation({
-    summary: 'Удаление заявки',
+    summary: 'Отклонение заявки',
     description:
       'Пользователь может удалить только свою исходящую заявку, в противном случае вернётся ошибка 403. Админ может удалить любую заявку.',
   })
